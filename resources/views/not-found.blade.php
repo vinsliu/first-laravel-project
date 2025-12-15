cd983996d6825059372817c5eb72a2142316c826
Vincent <vincent00lav@outlook.com>
2025-12-15 14:10:55 +0100

    ACTIVITE 8 : Créer une vue pour la page 404

---
+++ resources/views/not-found.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Page non trouvée</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [IndexController::class, 'index'])->name('home');
 
 Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.details');
+
+Route::fallback(function () {
+    return view('not-found');
+});
