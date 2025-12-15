d6dc96b40acb9c7dce9d4466f123bed713ef86ce
Vincent <vincent00lav@outlook.com>
2025-12-15 14:03:04 +0100

    ACTIVITE 7 : Nommer les routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index']);
+Route::get('/', [IndexController::class, 'index'])->name('home');
 
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.details');
