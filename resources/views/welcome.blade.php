45b1d08b5fa6966b750b0f01d3c3bc521572eef7
Vincent <vincent00lav@outlook.com>
2025-12-15 15:17:31 +0100

    ACTIVITE 11 : Créer un composant

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,4 +8,10 @@
 <h2>Bienvenue sur la page d’accueil</h2>
 <p>Ceci est le contenu principal de la page.</p>
 
+<x-article title="L’IA soigne mieux" description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." />
+
+<x-article title="Villes vertes" description="Les métropoles deviennent plus écologiques et durables." />
+
+<x-article title="Télétravail" description="Plus de liberté, mais aussi plus de solitude." />
+
 @endsection
