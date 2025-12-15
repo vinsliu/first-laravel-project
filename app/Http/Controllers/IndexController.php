75cab59057dd988cf26df79a6f2717c8b0c73542
Vincent <vincent00lav@outlook.com>
2025-12-15 13:30:07 +0100

    ACTIVITE 5 : Rendre dynamiquement la page d’accueil

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,6 +8,6 @@ class IndexController extends Controller
 {
     public static function index()
     {
-        return view('welcome');
+        return view('welcome', ['name' => 'Vincent']);
     }
 }
