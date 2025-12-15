b2c83ebf224b21ea3f64954c818a5e868c6bbcf5
Vincent <vincent00lav@outlook.com>
2025-12-15 17:02:24 +0100

    ACTIVITE 16 : Importer le CSS et le JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
