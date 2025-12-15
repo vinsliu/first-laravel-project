536a3d34591af586c3ee70f8dcbef7dad4f48ec7
Vincent <vincent00lav@outlook.com>
2025-12-15 15:56:46 +0100

    ACTIVITE 12 : Rendre les articles via une boucle

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,6 +8,12 @@ class IndexController extends Controller
 {
     public static function index()
     {
-        return view('welcome', ['name' => 'Vincent']);
+        $articles = [
+            'L’IA soigne mieux' => 'L’intelligence artificielle aide les médecins à diagnostiquer plus vite.',
+            'Villes vertes' => 'Les métropoles deviennent plus écologiques et durables.',
+            'Télétravail' => 'Plus de liberté, mais aussi plus de solitude.'
+        ];
+
+        return view('welcome', ['articles' => $articles]);
     }
 }
