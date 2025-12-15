c8cb87cd9c4df705c1e436f090616ef3e079a33f
Vincent <vincent00lav@outlook.com>
2025-12-15 16:23:56 +0100

    ACTIVITE 13 : Ne plus rendre le dernier article

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,9 @@
 <p>Ceci est le contenu principal de la page.</p>
 
 @foreach ($articles as $title => $description)
+@if ($loop->last)
+@break
+@endif
 <x-article title="{{ $title }}" description="{{ $description }}" />
 @endforeach
 
