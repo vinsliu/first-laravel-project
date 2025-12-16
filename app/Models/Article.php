08ebffb899ed972046f7f538da086db9dc1027c0
Vincent <vincent00lav@outlook.com>
2025-12-16 15:21:39 +0100

    Correction, code inutile retiré

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    protected $table = 'articles';
-    protected $primaryKey = 'id';
     protected $fillable = ['title', 'description'];
 }
